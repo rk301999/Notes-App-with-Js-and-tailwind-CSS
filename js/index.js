763fb7c6d0c749e51fe103d26c47dbd8a6181ce4
rk301999 <rk301999@gmail.com>
2022-04-22 00:58:12 +0530

    added the delete notes function too !

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+showNotes();
 let addBtn = document.getElementById("addBtn");
 console.log(addBtn);
 addBtn.addEventListener("click",function(e){
@@ -30,12 +31,28 @@ function showNotes(){
         html+= `<div class="cards border-2 ">
         <p class="text-sm py-2"> Add note ${index + 1}</p>
         <p class="py-2"> ${element}</p>
-        <button class=" bg-black text-sm text-white px-2 py-1 rounded-lg hover:bg-gray-700 ">Delete Note </button>
+        <button id="${index}" onclick="deleteNote(this.id)" class=" bg-black text-sm text-white px-2 py-1 rounded-lg hover:bg-gray-700 ">Delete Note </button>
     </div>`
     });
 
     let notesElm = document.getElementById('notes');
-    if(notes.length != 0){
+    if(notesObj.length != 0){
         notesElm.innerHTML = html;
     }
+    else{
+        notesElm.innerHTML = `nothing to show rn  `;
+ 
+    }
 }
+    function deleteNote(index) {
+        let notes = localStorage.getItem("notes");
+        if(notes == 0){
+            notesObj =[];
+        }
+        else{
+            notesObj = JSON.parse(notes);
+        }
+        notesObj.splice(index,1);
+        localStorage.setItem("notes",JSON.stringify(notesObj));
+        showNotes();
+    }
